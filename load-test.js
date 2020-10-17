4f0f6a53631dcbe22be09a135fc41afad3ff3a0e
Ibrahim <ibrahimpmahmood@gmail.com>
2020-10-18 00:38:43 +0100

    update load-test url

--- load-test.js
+++ load-test.js
@@ -2,7 +2,7 @@ import http from "k6/http";
 import { check, sleep } from 'k6';
 
 export default function() {
-    let response = http.get("https://rpul1rc6d3.execute-api.eu-west-2.amazonaws.com/dev/2aee4051-94e0-494b-8a3d-f03954fa0556");
+    let response = http.get("LAMBDA_URL");
     check(response, {
         'status is 200': (r) => r.status === 202
     })
